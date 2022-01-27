80390a16135e2a8bde96bb5e4572339648164849
PabloMunozMedina <93193033+PabloMunozMedina@users.noreply.github.com>
2022-01-27 19:23:35 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Cabecera @anadir="appAnadir" ></Cabecera>
+    <Listatareas ref="lista"></Listatareas>  
+
+  </div>
+</template>
+
+<script>
+import Cabecera from './components/Cabecera.vue'
+import Listatareas from './components/lista_tareas.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    Cabecera,
+    Listatareas
+  },
+  data(){
+    return{
+    
+    }
+  },
+  methods:{
+    appAnadir(tarea){
+        this.$refs.lista.anadirALista(tarea);
+      },
+  }
+}
+
+</script>
+
+<style scoped>
+#app{
+  background-color:mediumpurple;
+  border: 2px solid #ccc;
+  border-radius: 20px;
+}
+</style>
